b5182c87ed4c8de117666b950d914b8d894186f2
Mark Jones <mark@getkahoot.com>
2016-10-07 16:34:11 +0100

    Chapter 3 code

---
+++ chapter3/src/Data/AddressBook.purs
@@ -0,0 +1,43 @@
+module Data.AddressBook where
+
+import Prelude
+
+import Control.Plus (empty)
+import Data.List (List(..), filter, head)
+import Data.Maybe (Maybe)
+
+type Entry =
+  { firstName :: String
+  , lastName  :: String
+  , address   :: Address
+  }
+
+type Address =
+  { street :: String
+  , city   :: String
+  , state  :: String
+  }
+
+type AddressBook = List Entry
+
+showEntry :: Entry -> String
+showEntry entry = entry.lastName <> ", " <>
+                  entry.firstName <> ": " <>
+                  showAddress entry.address
+
+showAddress :: Address -> String
+showAddress addr = addr.street <> ", " <>
+                   addr.city <> ", " <>
+                   addr.state
+
+emptyBook :: AddressBook
+emptyBook = empty
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = Cons
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName = head <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
