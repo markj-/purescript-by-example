760001e4c240d97cec66655fedc8f107295cdc90
Mark Jones <mark@getkahoot.com>
2016-10-10 13:15:10 +0100

    chapter 3 exercise 2

--- chapter3/src/Data/AddressBook.purs
+++ chapter3/src/Data/AddressBook.purs
@@ -41,3 +41,9 @@ findEntry firstName lastName = head <<< filter filterEntry
   where
     filterEntry :: Entry -> Boolean
     filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.address.street == street
