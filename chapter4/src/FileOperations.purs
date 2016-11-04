e26896c157a3baba389f9d9533390fa9a1aa263c
Mark Jones <mark@getkahoot.com>
2016-11-04 14:48:26 +0000

    Write a function onlyFiles which returns all files (not directories) in all subdirectories of a directory.

--- chapter4/src/FileOperations.purs
+++ chapter4/src/FileOperations.purs
@@ -2,7 +2,7 @@ module FileOperations where
 
 import Prelude
 import Data.Path
-import Data.Array (concatMap, (:))
+import Data.Array (concatMap, (:), filter)
 
 allFiles :: Path -> Array Path
 allFiles file = file : concatMap allFiles (ls file)
@@ -11,3 +11,6 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+onlyFiles :: Path -> Array Path
+onlyFiles = filter (not isDirectory) <<< allFiles
